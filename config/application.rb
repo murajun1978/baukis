26250e32bcc7f61325f57bba08250a20fdc0d2cb
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2014-08-24 11:32:28 +0900

    ジェネレータの設定

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,16 @@ module Baukis
     config.i18n.load_path +=
       Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
   end
 end
