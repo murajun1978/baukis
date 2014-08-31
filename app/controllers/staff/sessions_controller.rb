cee20a638a6f77652a986c2b72dbf1094483c19d
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2014-08-31 16:22:34 +0900

    ユーザ認証(2)

---
+++ app/controllers/staff/sessions_controller.rb
@@ -0,0 +1,25 @@
+class Staff::SessionsController < Staff::Base
+  def new
+    if current_staff_member
+      redirect_to :staff_root
+    else
+      @form= Staff::LoginForm.new
+      render action: 'new'
+    end
+  end
+
+  def create
+    @form = Staff::LoginForm.new(params[:staff_login_form])
+    if staff_member = StaffMember.find_by(email_for_index: @form.email.try(:downcase))
+      session[:staff_member_id] = staff_member.id
+      redirect_to :staff_root
+    else
+      render action: 'new'
+    end
+  end
+
+  def destroy
+    session.delete(:staff_member_id)
+    redirect_to :staff_root
+  end
+end
